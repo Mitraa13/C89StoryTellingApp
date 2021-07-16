51a5b8d39273fb1163b59de000f5c73a43c85a51
Mitraa <72913943+Mitraa13@users.noreply.github.com>
2021-07-16 20:59:42 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/app';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBRn58CRHlMbHdh3OI2vhh8XRlky-AqPJc",
+  authDomain: "storytellingapp-1b38e.firebaseapp.com",
+  databaseURL: "https://storytellingapp-1b38e-default-rtdb.firebaseio.com",
+  projectId: "storytellingapp-1b38e",
+  storageBucket: "storytellingapp-1b38e.appspot.com",
+  messagingSenderId: "172773233643",
+  appId: "1:172773233643:web:a553228aba95efa5307fae"
+};
